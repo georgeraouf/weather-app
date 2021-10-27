b01000a9e733b8775030052352823d666a74c28d
George Raouf <george.raouf.ramses@gmail.com>
2021-10-27 09:20:56 +0200

    Callback Chaining

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
+address = process.argv[2];
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-});
+if(address) {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error);
+        }
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
+            
+            console.log(data.location);
+            console.log(forecastData);
+        });    
+    });  
+}
+else {
+    console.log('No address is provided!');
+}
 
-forecast(44.1545, -75.7088, (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-});
-  
