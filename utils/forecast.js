e17e4ad044fa95b10abbd79b9a89b170b97fd1c3
George Raouf <george.raouf.ramses@gmail.com>
2021-10-27 11:26:13 +0200

    Destruturing and property shorthand

--- utils/forecast.js
+++ utils/forecast.js
@@ -7,13 +7,13 @@ const forecast = (latitude, longitude, callback) => {
     longitude +
     '&units=m';
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find the location!', undefined);
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.');
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.');
         }
     })
 }
